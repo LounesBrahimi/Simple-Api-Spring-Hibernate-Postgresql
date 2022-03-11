2d03e550403f4d1a898e609098d92f8209fbb22e
LounesBrahimi <lounesnewgs5@gmail.com>
2022-03-11 13:26:02 +0100

    structure of the web app

---
+++ webapphibernate/src/main/java/com/webapphibernate/webapphibernate/WebapphibernateApplication.java
@@ -0,0 +1,13 @@
+package com.webapphibernate.webapphibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebapphibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebapphibernateApplication.class, args);
+	}
+
+}
